8ee1f7a3ae01b03fa2c2ba7ec0fc5bcf0f65697b
Samuel-Fagundes-de-Souza <samuelfagundes.sza@gmail.com>
2025-10-13 14:41:18 -0300

    Add files via upload

---
+++ lista7/AlunoIdade.java
@@ -0,0 +1,13 @@
+package prjAula07;
+
+public class AlunoIdade {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		double
+		,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+		.
+
+	}
+
+}
