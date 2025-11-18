1605483e10be96909eee4d4c85decb846a3e9173
Samuel Fagundes de Souza <samuelfagundes.sza@gmail.com>
2025-11-18 19:14:24 -0300

    Refactor ProgramaIntersecao to find intersections
    Updated the intersection program to store common elements in a new array.

--- listaRemota/ProgramaIntersecao.java
+++ listaRemota/ProgramaIntersecao.java
@@ -6,37 +6,36 @@ public class ProgramaIntersecao {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner (System.in);
-		
-		int a[], b[], i;
-		double c[];
-		final int TAM = 10;
+
+		int a[], b[], c[], i, j=0;
+        int TAM = 10;
 		a = new int [TAM];
 		b = new int [TAM];
-		c = new double [TAM];
-		
+		c = new int [TAM];
+
 		for (i=0; i<TAM; i++) {
-			System.out.println("Insira o "+ (i + 1) +"º elemento do conjunto A:");
+			System.out.println("Insira o "+ (i + 1) +"Âº elemento do conjunto A:");
 			a[i] = in.nextInt();
 		}
-		
+
 		for (i=0; i<TAM; i++) {
-			System.out.println("Insira o "+ (i + 1) +"º elemento do conjunto B:");
+			System.out.println("Insira o "+ (i + 1) +"Âº elemento do conjunto B:");
 			b[i] = in.nextInt();
+			b[j] = b[i];
 		}	
-		
+
 		for (i=0; i<TAM; i++) {
-			if (a[i] == b[i]) {
-				c[i] = a[i];
-			} else {
-				c[i] = 0.1;
+			for (j=0; j<TAM; j++) {
+				if (a[i] == b[j]) {
+					c[i] = a[i];
+				}
 			}
+
 		}
-		
-		System.out.print("Interseção C [ ");
+
+		System.out.print("InterseÃ§Ã£o C [ ");
 		for(i=0; i<TAM; i++) {
-			if (c[i] != 0.1) {
 				System.out.print(c[i]+" ");
-			}	
 		}
 		System.out.print("] ");
 	}
